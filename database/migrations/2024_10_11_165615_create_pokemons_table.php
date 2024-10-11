6419c4add37de24d8ba4929766ecf9aee0935c30
Yuri Peixoto <yuripeixoto@gmail.com>
2024-10-11 13:08:52 -0400

    feat: create pokemons table

---
+++ database/migrations/2024_10_11_165615_create_pokemons_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('pokemons', function (Blueprint $table) {
+            $table->id();
+            $table->integer('pokedex_number');
+            $table->string('name');
+            $table->integer('cp');
+            $table->integer('hp');
+            $table->float('weight');
+            $table->float('height');
+            $table->boolean('is_shiny')->default(false);
+            $table->string('image_url')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('pokemons');
+    }
+};
